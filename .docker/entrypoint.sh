5b4ba573e4d3efc41bced67ade46dd9e1968105e
Valteir Costa de Sousa <valteircosta@gmail.com>
2019-11-20 22:55:29 -0300

    Add ajustes para funcionar

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Aqui devemos rodar o compose  pois aqui o que é realizado fica no volume compartilhado
+# a imagem já está criada.
 
+composer install
+php artisan key:generate
 php artisan migrate
 php-fpm
