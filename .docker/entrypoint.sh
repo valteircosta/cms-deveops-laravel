1a8cd2734cfd6aed61264d31d34e483dbbfbf97f
valteir costa de sousa <valteircosta@gmail.com>
2019-12-19 08:02:40 -0300

    Add descrição entrypointer '

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -3,6 +3,9 @@
 # a imagem já está criada.
 echo "Concede permissão na pasta....."
 chmod -R 775 storage/
+chmod -R 775 storage/framework
+chmod -R 775 storage/logs
+chmod -R 775 bootstrap/cache
 echo "Instalando dependencias php....."
 composer install
 echo "Instalando dependencias node....."
