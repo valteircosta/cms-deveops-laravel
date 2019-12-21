c3c9f548ee6880a4bd8a207a23321235057c7494
valteir costa de sousa <valteircosta@gmail.com>
2019-12-21 15:09:14 -0300

    Update enrypoint.sh

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 # Aqui devemos rodar o compose  pois aqui o que é realizado fica no volume compartilhado
 # a imagem já está criada.
-echo "Concede permissão na pasta....."
-chmod -R 775 storage/
-chmod -R 775 storage/framework
-chmod -R 775 storage/logs
-chmod -R 775 bootstrap/cache
 echo "Instalando dependencias php....."
 composer install
 echo "Instalando dependencias node....."
 npm install
+echo "Concede permissão na pasta....."
+find storage bootstrap/cache public -type f -exec chmod o+w {} \;
+find storage bootstrap/cache public -type d -exec chmod o+wx {} \;
+chmod o-w public/index.php
 echo "Executando key:Generate....."
 php artisan key:generate
 echo "Executando migrate....."
